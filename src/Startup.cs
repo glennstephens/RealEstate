9dab92f281327f4f4cd978180c0be22e81569b89
René Ruppert <rene.ruppert@gmail.com>
2019-08-06 12:03:31 +0200

    Fix startup error when running on Azure

--- src/Startup.cs
+++ src/Startup.cs
@@ -79,7 +79,7 @@ namespace RealEstate
 			}
 			else
 			{
-				app.UseExceptionHandler("~/Error");
+				app.UseExceptionHandler("/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
